d61c3035ef403c782201063460a523961a2a4d17
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-03-19 13:16:00 +0700

    merubah master

--- Program.cs
+++ Program.cs
@@ -6,28 +6,11 @@ namespace JurnalMod4
     class Program
     {
         static void Main(string[] args)
+        {
+            // memanggil generic method
 
-    {
-        // memanggil generic method
-        Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(12, 34, 56));
-
-        
+            // memvalidasi generic class
+  
+        }
     }
-}
-
-class Penjumlahan
-{
-    public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
-    {
-        dynamic a = input1;
-        dynamic b = input2;
-        dynamic c = input3;
-
-        return a + b + c;
-
-        // atau opsi lain tanpa variable temporary:
-        //return (dynamic)input1 + (dynamic)input2 + (dynamic)input3;
-    }
-}
-
 }
