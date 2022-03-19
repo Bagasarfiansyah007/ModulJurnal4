67835fbfce802d298ec3262c531d7555a88457ff
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-03-19 13:02:28 +0700

    perubahan method

--- Program.cs
+++ Program.cs
@@ -10,12 +10,7 @@ namespace JurnalMod4
         // memanggil generic method
         Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(12, 34, 56));
 
-        // memvalidasi generic class
-        SimpleDataBase<double> db = new SimpleDataBase<double>();
-        db.AddNewData(12);
-        db.AddNewData(34);
-        db.AddNewData(56);
-        db.PrintAllData();
+        
     }
 }
 
@@ -34,30 +29,5 @@ class Penjumlahan
     }
 }
 
-class SimpleDataBase<T>
-{
-    private List<T> storedData;
-    private List<DateTime> inputDates;
 
-    public SimpleDataBase()
-    {
-        this.storedData = new List<T>();
-        this.inputDates = new List<DateTime>();
-    }
-
-    public void AddNewData(T newData)
-    {
-        this.inputDates.Add(DateTime.Now);
-        this.storedData.Add(newData);
-    }
-
-    public void PrintAllData()
-    {
-        for (int i = 0; i < this.inputDates.Count; i++)
-        {
-            Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
-                ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
-        }
-    }
-}
 }
